7dc3b20280288e4fcec40e4e762efb2aa85d5d6b
Andy Luu <andyluu324@gmail.com>
2021-12-20 20:17:16 -0500

    updated html, css, and js

--- app/js/script.js
+++ app/js/script.js
@@ -1,1 +1,22 @@
-console.log("Hello world");
+const menuButton = document.querySelector("#menuButton");
+const header = document.querySelector(".header");
+const overlay = document.querySelector(".overlay");
+
+menuButton.addEventListener("click", function(){
+
+    if(header.classList.contains("open")){ // Close Menu
+
+        header.classList.remove("open");
+        overlay.classList.remove("fade-in");
+        overlay.classList.add("fade-out");
+
+
+    }
+    else{ // Open Menu
+
+        header.classList.add("open");
+        overlay.classList.add("fade-in");
+        overlay.classList.remove("fade-out");
+
+    }
+})
