47f48c05c8a76283bf6a8938b5a92ff6d57fc154
andyluu1-admin <andyluu324@gmail.com>
2022-12-21 15:42:04 -0500

    finished header for mobile styling Al-13 #time 2h

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -24,11 +24,11 @@ const Header = (props: headerProps) => {
 
   // Generating all the nav links
   const links = headerLinks.map((link) => (
-    <div className="Header-link" onClick={ToggleNav} key={link.label}>
-      <a href={link.url}>
+    <a href={link.url} onClick={ToggleNav} key={link.label}>
+      <div className="Header-Link text-lg font-semibold w-full pt-4 pb-4 pl-4 hover:bg-brand-tertiary hover:border-2 hover:rounded-xl">
         {link.label}
-      </a>
-    </div>
+      </div>
+    </a>
   ));
 
   return (
@@ -37,19 +37,16 @@ const Header = (props: headerProps) => {
       pr-2 py-2 md:flex-nowrap">
         <div className="Header-Nav order-1 w-1/4 mt-auto mb-auto pl-4 md:hidden" 
         onClick={ToggleNav}>
-            <div className="Header-NavSegment w-10 h-1 bg-brand-secondary 
-            rounded"></div>
-            <div className="Header-NavSegment w-10 h-1 bg-brand-secondary 
-            rounded mt-1.5 mb-1.5"></div>
-            <div className="Header-NavSegment w-10 h-1 bg-brand-secondary 
-            rounded"></div>
+            <div className={"Header-NavSegment w-10 h-1 bg-brand-secondary rounded " + (isNavActive ? "ease duration-300 transform rotate-45 translate-y-3" : "duration-300")}></div>
+            <div className={"Header-NavSegment w-10 h-1 bg-brand-secondary rounded mt-2 mb-2 " + (isNavActive ? "ease duration-300 transform opacity-0" : "duration-300")}></div>
+            <div className={"Header-NavSegment w-10 h-1 bg-brand-secondary rounded "+ (isNavActive ? "ease duration-300 transform -rotate-45 -translate-y-3" : "duration-300")}></div>
         </div>
         <div className="Header-Logo order-2 w-1/2 text-center mt-auto 
-        mb-auto text-2xl  md:order-1 md:w-1/3">
+        mb-auto text-4xl font-extrabold md:order-1 md:w-1/3">
             Andy Luu
         </div>
         <div className={"Header-Links order-4 w-full md:flex md:order-2 " + 
-        (isNavActive ? "ml-0 mt-12 absolute bg-brand-primary" : "hidden")}>
+        (isNavActive ? "ml-0 mt-14 absolute bg-brand-primary border-4 border-brand-primary rounded-b-xl" : "hidden")}>
           {links}
         </div>
         <div className="Header-ImageContainer order-3 w-1/4 mt-auto mb-auto">
