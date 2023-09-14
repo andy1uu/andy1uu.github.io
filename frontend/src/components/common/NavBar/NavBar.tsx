aa886109930c164e26468a98b58bdd5247985763
Andy Luu <andyluu324@gmail.com>
2023-09-14 16:17:21 -0400

    Removed currently unused buttons

--- frontend/src/components/common/NavBar/NavBar.tsx
+++ frontend/src/components/common/NavBar/NavBar.tsx
@@ -1,16 +1,16 @@
 import * as React from "react";
 import NavBarIcon from "./NavBarIcon";
-import { FaHouseChimney, FaUserLarge, FaGraduationCap } from "react-icons/fa6";
+import { FaHouseChimney } from "react-icons/fa6";
 
 const NavBar = () => (
   <div className="NavBar left-0 top-0 w-16 flex-col bg-darkBackground shadow-lg lg:flex">
     <NavBarIcon icon={<FaHouseChimney size={24} />} text="Home" link="" />
-    <NavBarIcon icon={<FaUserLarge size={24} />} text="About" link="about" />
+    {/*     <NavBarIcon icon={<FaUserLarge size={24} />} text="About" link="about" />
     <NavBarIcon
       icon={<FaGraduationCap size={24} />}
       text="Education"
       link="education"
-    />
+    /> */}
   </div>
 );
 
