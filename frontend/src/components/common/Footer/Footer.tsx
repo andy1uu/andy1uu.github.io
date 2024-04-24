fc2c53139014a0a8d642e9ae02d6d6484fc1785d
Andy Luu <andyluu324@gmail.com>
2024-04-24 14:12:02 -0400

    feat(Website-Template): Put my website template into the github.io repo

--- frontend/src/components/common/Footer/Footer.tsx
+++
@@ -1,9 +0,0 @@
-import * as React from "react";
-
-const Footer = () => (
-  <div className="Footer mt-auto flex w-full justify-center">
-    &copy; 2023 Andy Luu. All Rights Reserved.
-  </div>
-);
-
-export default Footer;
