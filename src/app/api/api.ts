9458a0655132715a0689bb6a8758dfa558fd6f31
Andy Luu <andyluu324@gmail.com>
2024-05-06 11:51:57 -0400

    fix(Middleware): deleted middleware to see waht happens

--- src/app/api/api.ts
+++ src/app/api/api.ts
@@ -4,6 +4,5 @@ export default axios.create({
   baseURL: process.env.API_URL,
   headers: {
     "Content-Type": "application/json",
-    "API-Key": process.env.API_KEY,
   },
 });
--- src/app/api/config/limiter.ts
+++
@@ -1,7 +0,0 @@
-import { RateLimiter } from "limiter";
-
-export const limiter = new RateLimiter({
-  tokensPerInterval: 1000,
-  interval: "min",
-  fireImmediately: true,
-});
